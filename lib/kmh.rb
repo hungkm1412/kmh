78168da23372ec5ccb555db155539d5e97a486a0
hungkm <hungkm@hungkm-H61M-D2P-B3.(none)>
2014-05-08 17:07:15 +0700

    Hello gem

---
+++ lib/kmh.rb
@@ -0,0 +1,8 @@
+require "kmh/version"
+
+module Kmh
+  # Your code goes here...
+  def self.hi
+    puts "Xin chao, toi ten la Hung!"
+  end
+end
---
+++ lib/kmh/version.rb
@@ -0,0 +1,3 @@
+module Kmh
+  VERSION = "0.0.1"
+end
